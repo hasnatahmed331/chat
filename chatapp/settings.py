de910f904cc0272f0667cd7f09ff274301b74d83
hasnatahmed331 <hasnatahmed331@gmail.com>
2023-05-18 11:14:03 +0500

    erro_fixed

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -144,4 +148,5 @@ AUTHENTICATION_BACKENDS = [
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
+
 
